d59c54a7d0c6943c5cb9dee610ce97dc94d23871
rgvanwesep <rvanwes@gmail.com>
2024-12-11 20:30:22 +0000

    feat: add recursive function for day 11

--- 2024/day11/pebbles.go
+++ 2024/day11/pebbles.go
@@ -27,33 +27,39 @@ func Join(digits []int) int {
 	return x
 }
 
+func CountAncestors(pebble int, nBlinks int) int {
+	if nBlinks == 0 {
+		return 1
+	}
+	digits := Split(pebble)
+	nDigits := len(digits)
+	newPebbles := make([]int, 0, 2)
+	switch {
+	case pebble == 0:
+		newPebbles = append(newPebbles, 1)
+	case nDigits%2 == 0:
+		newPebbles = append(newPebbles, Join(digits[:nDigits/2]), Join(digits[nDigits/2:]))
+	default:
+		newPebbles = append(newPebbles, pebble*2024)
+	}
+	count := 0
+	for _, p := range newPebbles {
+		count += CountAncestors(p, nBlinks - 1)
+	}
+	return count
+}
+
 func CountPebbles(inputs []string) int {
 	const nBlinks = 25
 	input := inputs[0]
 	split := strings.Split(input, " ")
-	pebbles := make([]int, len(split))
-	for i, s := range split {
+	count := 0
+	for _, s := range split {
 		if pebble, err := strconv.Atoi(s); err == nil {
-			pebbles[i] = pebble
+			count += CountAncestors(pebble, nBlinks)
 		} else {
 			log.Panicf("Could not parse %q", input)
 		}
 	}
-	for range nBlinks {
-		newPebbles := []int{}
-		for _, pebble := range pebbles {
-			digits := Split(pebble)
-			nDigits := len(digits)
-			switch {
-			case pebble == 0:
-				newPebbles = append(newPebbles, 1)
-			case nDigits%2 == 0:
-				newPebbles = append(newPebbles, Join(digits[:nDigits/2]), Join(digits[nDigits/2:]))
-			default:
-				newPebbles = append(newPebbles, pebble*2024)
-			}
-		}
-		pebbles = newPebbles
-	}
-	return len(pebbles)
+	return count
 }
