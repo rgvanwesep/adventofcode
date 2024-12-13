9b9d4fdd72151e57d7ffcbf146a0346f79e91ddb
rgvanwesep <rvanwes@gmail.com>
2024-12-13 00:12:55 +0000

    fix: finish part 1 implementation

--- 2024/day12/garden.go
+++ 2024/day12/garden.go
@@ -1,11 +1,17 @@
 package day12
 
-import "log"
-
-var regionId int
+import (
+	"iter"
+	"log"
+)
 
 type Point struct {
 	x, y int
+}
+
+func (p Point) IsNeighbor(other Point) bool {
+	sqDist := (p.x-other.x)*(p.x-other.x) + (p.y-other.y)*(p.y-other.y)
+	return sqDist == 1
 }
 
 type Grid[T any] struct {
@@ -39,6 +45,17 @@ func (g *Grid[T]) SetValue(p Point, v T) (ok bool) {
 	return
 }
 
+func (g *Grid[T]) All() iter.Seq2[Point, T] {
+	iToP := func(i int) Point {
+		row := i / g.ncols
+		return Point{i % g.ncols, g.nrows - row - 1}
+	}
+	return func(yield func(Point, T) bool) {
+		for i := 0; i < len(g.values) && yield(iToP(i), g.values[i]); i++ {
+		}
+	}
+}
+
 type Queue[T any] struct {
 	values []T
 }
@@ -60,6 +77,30 @@ func (q *Queue[T]) Pop() (value T, ok bool) {
 
 func (q *Queue[T]) Push(value T) {
 	q.values = append(q.values, value)
+}
+
+type Stack[T any] struct {
+	values []T
+}
+
+func NewStack[T any]() *Stack[T] {
+	return &Stack[T]{
+		values: []T{},
+	}
+}
+
+func (s *Stack[T]) Pop() (value T, ok bool) {
+	size := len(s.values)
+	if size > 0 {
+		value = s.values[size-1]
+		s.values = s.values[:size-1]
+		ok = true
+	}
+	return
+}
+
+func (s *Stack[T]) Push(value T) {
+	s.values = append(s.values, value)
 }
 
 type Plot struct {
@@ -86,21 +127,10 @@ func (m Mapper) Step(g *Grid[Plot]) []Mapper {
 		{m.position.x, m.position.y + 1},
 	}
 	mappers := []Mapper{}
-	newRegionFound := false
 	for _, neighbor := range neighbors {
 		if plot, ok := g.GetValue(neighbor); ok {
-			if plot.regionId == -1 {
-				var nextRegion Region
-				if plot.plant == m.region.plant {
-					nextRegion = m.region
-				} else {
-					if !newRegionFound {
-						newRegionFound = true
-						regionId++
-						nextRegion = Region{regionId, plot.plant}
-					}
-				}
-				mappers = append(mappers, Mapper{neighbor, nextRegion})
+			if plot.regionId == 0 && plot.plant == m.region.plant {
+				mappers = append(mappers, Mapper{neighbor, m.region})
 			}
 		}
 	}
@@ -114,7 +144,7 @@ func ParseGrid(inputs []string) *Grid[Plot] {
 		for col := range input {
 			p := Point{col, nrows - row - 1}
 			value := input[col]
-			plot := Plot{plant: value, regionId: -1}
+			plot := Plot{plant: value}
 			if ok := grid.SetValue(p, plot); !ok {
 				log.Panicf("Could not set value for %v", p)
 			}
@@ -123,20 +153,61 @@ func ParseGrid(inputs []string) *Grid[Plot] {
 	return grid
 }
 
-func SumFencePrice(inputs []string) int {
-	grid := ParseGrid(inputs)
-	mappers := NewQueue[Mapper]()
-	plot, _ := grid.GetValue(Point{0,0})
-	mappers.Push(Mapper{position: Point{0, 0}, region: Region{regionId, plot.plant}})
-	for {
-		if mapper, ok := mappers.Pop(); ok {
-			newMappers := mapper.Step(grid)
-			for _, m := range newMappers {
-				mappers.Push(m)
+func AssignRegions(grid *Grid[Plot]) int {
+	regionId := 1
+	for p, plot := range grid.All() {
+		if plot.regionId == 0 {
+			mappers := NewStack[Mapper]()
+			mappers.Push(Mapper{position: p, region: Region{regionId, plot.plant}})
+			for {
+				if mapper, ok := mappers.Pop(); ok {
+					newMappers := mapper.Step(grid)
+					for _, m := range newMappers {
+						mappers.Push(m)
+					}
+				} else {
+					break
+				}
 			}
-		} else {
-			break
+			regionId++
 		}
 	}
-	return 0
+	return regionId - 1
+}
+
+func GetRegionPoints(grid *Grid[Plot], regionId int) []Point {
+	points := []Point{}
+	for p, plot := range grid.All() {
+		if plot.regionId == regionId {
+			points = append(points, p)
+		}
+	}
+	return points
+}
+
+func GetPerimeter(points []Point) int {
+	perimeter := 4 * len(points)
+	for i, p1 := range points {
+		for _, p2 := range points[:i] {
+			if p1.IsNeighbor(p2) {
+				perimeter -= 2
+			}
+		}
+	}
+	return perimeter
+}
+
+func SumFencePrice(inputs []string) int {
+	sum := 0
+	grid := ParseGrid(inputs)
+	log.Printf("Grid has %d points", grid.nrows*grid.ncols)
+	nRegions := AssignRegions(grid)
+	log.Printf("Grid has %d regions", nRegions)
+	for i := 1; i <= nRegions; i++ {
+		points := GetRegionPoints(grid, i)
+		area := len(points)
+		perimeter := GetPerimeter(points)
+		sum += area * perimeter
+	}
+	return sum
 }
