6697ab914c3636c1eb93ea1112d1607619cc9416
rgvanwesep <rvanwes@gmail.com>
2024-12-14 00:58:09 +0000

    fix: implement using equation solution

--- 2024/day13/claw.go
+++ 2024/day13/claw.go
@@ -13,6 +13,17 @@ const (
 	prizePattern  = `^Prize: X=(\d+), Y=(\d+)$`
 )
 
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
+
+func lcm(a, b int) int {
+	return a * b / gcd(a, b)
+}
+
 type vector struct {
 	x, y int
 }
@@ -21,10 +32,10 @@ type machine struct {
 	aVec, bVec, prizeVec vector
 }
 
-func parseMachines(inputs []string) []*machine {
+func parseMachines(inputs []string) []machine {
 	buttonMatcher := regexp.MustCompile(buttonPattern)
 	prizeMatcher := regexp.MustCompile(prizePattern)
-	machines := []*machine{}
+	machines := []machine{}
 	for i := 0; i < len(inputs); i += 4 {
 		matched := buttonMatcher.FindStringSubmatch(inputs[i])
 		x, err := strconv.Atoi(matched[2])
@@ -59,12 +70,30 @@ func parseMachines(inputs []string) []*machine {
 		}
 		prizeVec := vector{x, y}
 
-		machines = append(machines, &machine{aVec, bVec, prizeVec})
+		machines = append(machines, machine{aVec, bVec, prizeVec})
 	}
 	return machines
 }
 
 func MinCost(inputs []string) int {
 	sum := 0
+	machines := parseMachines(inputs)
+	for _, m := range machines {
+		lcmA := lcm(m.aVec.x, m.aVec.y)
+		xFact, yFact := lcmA/m.aVec.x, lcmA/m.aVec.y
+		xFactPrize, xFactB := xFact*m.prizeVec.x, xFact*m.bVec.x
+		numerB := xFactPrize - yFact*m.prizeVec.y
+		denom := xFactB - yFact*m.bVec.y
+		bRem := numerB % denom
+		if bRem == 0 {
+			b := numerB / denom
+			numerA := xFactPrize - xFactB*b
+			aRem := numerA % lcmA
+			if aRem == 0 {
+				a := numerA / lcmA
+				sum += a*aCost + b*bCost
+			}
+		}
+	}
 	return sum
 }
