de7150364c462900a195afbc4056ce4dd5a7b794
rgvanwesep <rvanwes@gmail.com>
2024-12-14 00:09:10 +0000

    fix: remove broken code

--- 2024/day13/claw.go
+++ 2024/day13/claw.go
@@ -11,84 +11,14 @@ const (
 	bCost         = 1
 	buttonPattern = `^Button (A|B): X\+(\d+), Y\+(\d+)$`
 	prizePattern  = `^Prize: X=(\d+), Y=(\d+)$`
-	maxUInt       = ^uint(0)
-	maxInt        = int(maxUInt >> 1)
 )
 
 type vector struct {
 	x, y int
 }
 
-func add(v1, v2 vector) vector {
-	return vector{v1.x + v2.x, v1.y + v2.y}
-}
-
-type grid struct {
-	xMax, yMax int
-	values     []int
-}
-
-func newGrid(xMax, yMax int) *grid {
-	size := xMax * yMax
-	values := make([]int, size)
-	for i := range size {
-		values[i] = maxInt
-	}
-	return &grid{xMax, yMax, values}
-}
-
-func (g *grid) get(v vector) (int, bool) {
-	i := v.y*g.xMax + v.x
-	if i < len(g.values) {
-		c := g.values[i]
-		if c != maxInt {
-			return g.values[i], true
-		}
-	}
-	return 0, false
-}
-
-func (g *grid) set(v vector, c int) bool {
-	i := v.y*g.xMax + v.x
-	if i < len(g.values) {
-		g.values[i] = c
-		return true
-	}
-	return false
-}
-
 type machine struct {
 	aVec, bVec, prizeVec vector
-	memo                 *grid
-}
-
-func (m *machine) minCost(origin vector) int {
-	if c, ok := m.memo.get(origin); ok {
-		return c
-	}
-	if origin.x == m.prizeVec.x && origin.y == m.prizeVec.y {
-		m.memo.set(origin, 0)
-		return 0
-	}
-	if origin.x > m.prizeVec.x || origin.y > m.prizeVec.y {
-		return maxInt
-	}
-	aNextOrigin := add(origin, m.aVec)
-	aNextCost := m.minCost(aNextOrigin)
-	bNextOrigin := add(origin, m.bVec)
-	bNextCost := m.minCost(bNextOrigin)
-	if aNextCost == maxInt && bNextCost == maxInt {
-		return maxInt
-	}
-	if aNextCost == maxInt {
-		return bCost + bNextCost
-	}
-	if bNextCost == maxInt {
-		return aCost + aNextCost
-	}
-	result := min(aCost+aNextCost, bCost+bNextCost)
-	m.memo.set(origin, result)
-	return result
 }
 
 func parseMachines(inputs []string) []*machine {
@@ -129,19 +59,12 @@ func parseMachines(inputs []string) []*machine {
 		}
 		prizeVec := vector{x, y}
 
-		machines = append(machines, &machine{aVec, bVec, prizeVec, newGrid(prizeVec.x, prizeVec.y)})
+		machines = append(machines, &machine{aVec, bVec, prizeVec})
 	}
 	return machines
 }
 
 func MinCost(inputs []string) int {
 	sum := 0
-	origin := vector{0, 0}
-	machines := parseMachines(inputs)
-	for _, m := range machines {
-		if c := m.minCost(origin); c != maxInt {
-			sum += c
-		}
-	}
 	return sum
 }
