8f6395aab85e94cc28588ea3e47de2663e2ccbf2
rgvanwesep <rvanwes@gmail.com>
2024-12-22 23:40:55 +0000

    fix: remove more unneeded code

--- 2024/day21/codes.go
+++ 2024/day21/codes.go
@@ -1,22 +1,5 @@
 package day21
 
-const (
-	upChar    = '^'
-	downChar  = 'v'
-	leftChar  = '<'
-	rightChar = '>'
-	pressChar = 'A'
-)
-
-type vector struct {
-	x, y int
-}
-
-type state struct {
-	position  vector
-	isPressed bool
-}
-
 func CalcComplexity(inputs []string) int {
 	return 0
 }
