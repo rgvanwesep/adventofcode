435bab7f530eb9d863ab40dd5e9cac06e9faba86
rgvanwesep <rvanwes@gmail.com>
2024-12-22 23:56:45 +0000

    feat: add some interfaces

--- 2024/day21/codes.go
+++ 2024/day21/codes.go
@@ -1,5 +1,27 @@
 package day21
 
+const (
+	upChar       = '^'
+	downChar     = 'v'
+	leftChar     = '<'
+	rightChar    = '>'
+	activateChar = 'A'
+)
+
+type presser interface {
+	move(direction byte)
+	press()
+}
+
+type controller interface {
+	signal(value byte)
+}
+
+type link struct {
+	c *controller
+	p *presser
+}
+
 func CalcComplexity(inputs []string) int {
 	return 0
 }
