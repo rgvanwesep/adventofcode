02c271495aa0d69c4ffe21ddfc5f1bee9de1b39d
rgvanwesep <rvanwes@gmail.com>
2024-12-23 05:41:45 +0000

    feat: implement valid edge computation

--- 2024/day21/codes.go
+++ 2024/day21/codes.go
@@ -158,7 +158,322 @@ func getAllStates() []string {
 	return states
 }
 
-func isValidEdge(from string, to string) bool {
+type robot interface {
+	getPostion() byte
+	setPosition(byte)
+	getNext() robot
+	setNext(robot)
+	move(byte) bool
+	press() bool
+}
+
+type directionalRobot struct {
+	position byte
+	next     robot
+}
+
+func (r *directionalRobot) getPostion() byte {
+	return r.position
+}
+
+func (r *directionalRobot) setPosition(position byte) {
+	r.position = position
+}
+
+func (r *directionalRobot) getNext() robot {
+	return r.next
+}
+
+func (r *directionalRobot) setNext(next robot) {
+	r.next = next
+}
+
+/*
+	+---+---+
+	| ^ | A |
+
++---+---+---+
+| < | v | > |
++---+---+---+
+*/
+func (r *directionalRobot) move(direction byte) bool {
+	switch [2]byte{r.position, direction} {
+	case [2]byte{'A', '^'}:
+		return false
+	case [2]byte{'A', 'v'}:
+		r.position = '>'
+		return true
+	case [2]byte{'A', '<'}:
+		r.position = '^'
+		return true
+	case [2]byte{'A', '>'}:
+		return false
+	case [2]byte{'^', '^'}:
+		return false
+	case [2]byte{'^', 'v'}:
+		r.position = 'v'
+		return true
+	case [2]byte{'^', '<'}:
+		return false
+	case [2]byte{'^', '>'}:
+		r.position = 'A'
+		return true
+	case [2]byte{'v', '^'}:
+		r.position = '^'
+		return true
+	case [2]byte{'v', 'v'}:
+		return false
+	case [2]byte{'v', '<'}:
+		r.position = '<'
+		return true
+	case [2]byte{'v', '>'}:
+		r.position = '>'
+		return true
+	case [2]byte{'<', '^'}:
+		return false
+	case [2]byte{'<', 'v'}:
+		return false
+	case [2]byte{'<', '<'}:
+		return false
+	case [2]byte{'<', '>'}:
+		r.position = 'v'
+		return true
+	case [2]byte{'>', '^'}:
+		r.position = 'A'
+		return true
+	case [2]byte{'>', 'v'}:
+		return false
+	case [2]byte{'>', '<'}:
+		r.position = 'v'
+		return true
+	case [2]byte{'>', '>'}:
+		return false
+	}
+	return false
+}
+
+func (r *directionalRobot) press() bool {
+	if r.next == nil {
+		return true
+	}
+	if r.position == 'A' {
+		return r.getNext().press()
+	}
+	return r.getNext().move(r.position)
+}
+
+var _ robot = &directionalRobot{}
+
+type numericalRobot struct {
+	position byte
+}
+
+func (r *numericalRobot) getPostion() byte {
+	return r.position
+}
+
+func (r *numericalRobot) setPosition(position byte) {
+	r.position = position
+}
+
+func (r *numericalRobot) getNext() robot {
+	return nil
+}
+
+func (r *numericalRobot) setNext(next robot) {
+}
+
+/*
++---+---+---+
+| 7 | 8 | 9 |
++---+---+---+
+| 4 | 5 | 6 |
++---+---+---+
+| 1 | 2 | 3 |
++---+---+---+
+
+	| 0 | A |
+	+---+---+
+*/
+func (r *numericalRobot) move(direction byte) bool {
+	switch [2]byte{r.position, direction} {
+	case [2]byte{'A', '^'}:
+		r.position = '3'
+		return true
+	case [2]byte{'A', 'v'}:
+		return false
+	case [2]byte{'A', '<'}:
+		r.position = '0'
+		return true
+	case [2]byte{'A', '>'}:
+		return false
+	case [2]byte{'0', '^'}:
+		r.position = '2'
+		return true
+	case [2]byte{'0', 'v'}:
+		return false
+	case [2]byte{'0', '<'}:
+		return false
+	case [2]byte{'0', '>'}:
+		r.position = 'A'
+		return true
+	case [2]byte{'1', '^'}:
+		r.position = '4'
+		return true
+	case [2]byte{'1', 'v'}:
+		return false
+	case [2]byte{'1', '<'}:
+		return false
+	case [2]byte{'1', '>'}:
+		r.position = '2'
+		return true
+	case [2]byte{'2', '^'}:
+		r.position = '5'
+		return true
+	case [2]byte{'2', 'v'}:
+		r.position = '0'
+		return true
+	case [2]byte{'2', '<'}:
+		r.position = '1'
+		return true
+	case [2]byte{'2', '>'}:
+		r.position = '3'
+		return true
+	case [2]byte{'3', '^'}:
+		r.position = '6'
+		return true
+	case [2]byte{'3', 'v'}:
+		r.position = 'A'
+		return true
+	case [2]byte{'3', '<'}:
+		r.position = '2'
+		return true
+	case [2]byte{'3', '>'}:
+		return false
+	case [2]byte{'4', '^'}:
+		r.position = '7'
+		return true
+	case [2]byte{'4', 'v'}:
+		r.position = '1'
+		return true
+	case [2]byte{'4', '<'}:
+		return false
+	case [2]byte{'4', '>'}:
+		r.position = '5'
+		return true
+	case [2]byte{'5', '^'}:
+		r.position = '8'
+		return true
+	case [2]byte{'5', 'v'}:
+		r.position = '2'
+		return true
+	case [2]byte{'5', '<'}:
+		r.position = '4'
+		return true
+	case [2]byte{'5', '>'}:
+		r.position = '6'
+		return true
+	case [2]byte{'6', '^'}:
+		r.position = '9'
+		return true
+	case [2]byte{'6', 'v'}:
+		r.position = '3'
+		return true
+	case [2]byte{'6', '<'}:
+		r.position = '5'
+		return true
+	case [2]byte{'6', '>'}:
+		return false
+	case [2]byte{'7', '^'}:
+		return false
+	case [2]byte{'7', 'v'}:
+		r.position = '4'
+		return true
+	case [2]byte{'7', '<'}:
+		return false
+	case [2]byte{'7', '>'}:
+		r.position = '8'
+		return true
+	case [2]byte{'8', '^'}:
+		return false
+	case [2]byte{'8', 'v'}:
+		r.position = '5'
+		return true
+	case [2]byte{'8', '<'}:
+		r.position = '7'
+		return true
+	case [2]byte{'8', '>'}:
+		r.position = '9'
+		return true
+	case [2]byte{'9', '^'}:
+		return false
+	case [2]byte{'9', 'v'}:
+		r.position = '6'
+		return true
+	case [2]byte{'9', '<'}:
+		r.position = '8'
+		return true
+	case [2]byte{'9', '>'}:
+		return false
+	}
+	return false
+}
+
+func (r *numericalRobot) press() bool {
+	return true
+}
+
+var _ robot = &numericalRobot{}
+
+type robotChain struct {
+	robots [3]robot
+}
+
+func newRobotChain(state string) *robotChain {
+	robots := [3]robot{
+		new(directionalRobot),
+		new(directionalRobot),
+		new(numericalRobot),
+	}
+	for i := range 3 {
+		robots[i].setPosition(state[i])
+	}
+	for i := range 2 {
+		robots[i].setNext(robots[i+1])
+	}
+	return &robotChain{robots}
+}
+
+func (r *robotChain) apply(input byte) bool {
+	if input == 'A' {
+		return r.robots[0].press()
+	}
+	return r.robots[0].move(input)
+}
+
+func (r *robotChain) getState() string {
+	return string([]byte{
+		r.robots[0].getPostion(),
+		r.robots[1].getPostion(),
+		r.robots[2].getPostion(),
+	})
+}
+
+func isValidWithInput(input byte, from, to string) bool {
+	robots := newRobotChain(from)
+	if ok := robots.apply(input); ok && robots.getState() == to {
+		return true
+	}
+	return false
+}
+
+func isValidEdge(from, to string) bool {
+	directionalValues := []byte{'^', 'v', '<', '>', 'A'}
+	for _, input := range directionalValues {
+		if isValidWithInput(input, from, to) {
+			return true
+		}
+	}
 	return false
 }
 
--- 2024/day21/codes_test.go
+++ 2024/day21/codes_test.go
@@ -29,3 +29,62 @@ func TestCalcComplexity(t *testing.T) {
 		}
 	}
 }
+
+func TestIsValidWithInput(t *testing.T) {
+	cases := []struct {
+		input    byte
+		from, to string
+		expected bool
+	}{
+		{
+			input:    '<',
+			from:     "AAA",
+			to:       "^AA",
+			expected: true,
+		},
+		{
+			input:    'v',
+			from:     "^AA",
+			to:       "vAA",
+			expected: true,
+		},
+		{
+			input:    'A',
+			from:     "vAA",
+			to:       "v>A",
+			expected: true,
+		},
+		{
+			input:    '<',
+			from:     "v>A",
+			to:       "<>A",
+			expected: true,
+		},
+		{
+			input:    'A',
+			from:     "<>A",
+			to:       "<vA",
+			expected: true,
+		},
+		{
+			input:    'A',
+			from:     "<vA",
+			to:       "<<A",
+			expected: true,
+		},
+		{
+			input:    '>',
+			from:     "<<A",
+			to:       "v<A",
+			expected: true,
+		},
+	}
+	for _, c := range cases {
+		result := isValidWithInput(c.input, c.from, c.to)
+		if result != c.expected {
+			t.Errorf("isValidWithInput(%q, %q, %q) == %t, expected %t",
+				c.input, c.from, c.to, result, c.expected,
+			)
+		}
+	}
+}
