8259024d03cb2ccfe87a896f55496d118e8aa727
rgvanwesep <rvanwes@gmail.com>
2025-01-08 20:08:16 +0000

    feat: add operations to set package

--- 2024/set/set.go
+++ 2024/set/set.go
@@ -1,6 +1,10 @@
 package set
 
-import "iter"
+import (
+	"fmt"
+	"iter"
+	"strings"
+)
 
 type Set[T comparable] interface {
 	Add(T)
@@ -10,12 +14,88 @@ type Set[T comparable] interface {
 	Contains(T) bool
 	Len() int
 	Remove(T)
+	String() string
 }
 
 func NewSet[T comparable]() Set[T] {
 	s := new(setMap[T])
 	s.values = map[T]struct{}{}
 	return s
+}
+
+func Equals[T comparable](x, y Set[T]) bool {
+	if x.Len() == y.Len() {
+		for v := range x.All() {
+			if !y.Contains(v) {
+				return false
+			}
+		}
+	} else {
+		return false
+	}
+	return true
+}
+
+func Intersection[T comparable](x, y Set[T]) Set[T] {
+	var left, right Set[T]
+	z := NewSet[T]()
+	if x.Len() < y.Len() {
+		left = x
+		right = y
+	} else {
+		left = y
+		right = x
+	}
+	for v := range left.All() {
+		if right.Contains(v) {
+			z.Add(v)
+		}
+	}
+	return z
+}
+
+func Union[T comparable](x, y Set[T]) Set[T] {
+	z := NewSet[T]()
+	for v := range x.All() {
+		z.Add(v)
+	}
+	for v := range y.All() {
+		z.Add(v)
+	}
+	return z
+}
+
+func Difference[T comparable](x, y Set[T]) Set[T] {
+	z := x.Clone()
+	for v := range y.All() {
+		z.Remove(v)
+	}
+	return z
+}
+
+func SymmetricDifference[T comparable](x, y Set[T]) Set[T] {
+	z := NewSet[T]()
+	for v := range x.All() {
+		if !y.Contains(v) {
+			z.Add(v)
+		}
+	}
+	for v := range y.All() {
+		if !x.Contains(v) {
+			z.Add(v)
+		}
+	}
+	return z
+}
+
+func CartesianProduct[T comparable](x, y Set[T]) Set[[2]T] {
+	z := NewSet[[2]T]()
+	for vx := range x.All() {
+		for vy := range y.All() {
+			z.Add([2]T{vx, vy})
+		}
+	}
+	return z
 }
 
 type setMap[T comparable] struct {
@@ -60,4 +140,14 @@ func (s *setMap[T]) Len() int {
 
 func (s *setMap[T]) Remove(value T) {
 	delete(s.values, value)
+}
+
+func (s setMap[T]) String() string {
+	parts := make([]string, s.Len())
+	i := 0
+	for v := range s.All() {
+		parts[i] = fmt.Sprintf("%v", v)
+		i++
+	}
+	return strings.Join([]string{"{", strings.Join(parts, ", "), "}"}, "")
 }
--- 2024/set/set_test.go
+++ 2024/set/set_test.go
@@ -1,16 +1,18 @@
 package set
 
 import (
+	"fmt"
 	"slices"
 	"testing"
 )
 
 func TestSet(t *testing.T) {
 	var (
-		i  int
-		b  byte
-		bs []byte
-		ok bool
+		i   int
+		b   byte
+		bs  []byte
+		str string
+		ok  bool
 	)
 	s := NewSet[byte]()
 
@@ -28,6 +30,10 @@ func TestSet(t *testing.T) {
 	}
 	if len(bs) != 0 {
 		t.Errorf("All() == %q, expected \"\"", string(bs))
+	}
+	str = fmt.Sprint(s)
+	if str != "{}" {
+		t.Errorf("String() == %q, expected \"{}\"", str)
 	}
 
 	s.Add('a')
@@ -49,6 +55,10 @@ func TestSet(t *testing.T) {
 	}
 	if len(bs) != 1 || bs[0] != 'a' {
 		t.Errorf("All() == %q, expected \"a\"", string(bs))
+	}
+	str = fmt.Sprint(s)
+	if str != fmt.Sprintf("{%v}", 'a') {
+		t.Errorf("String() == %q, expected %q", str, fmt.Sprintf("{%v}", 'a'))
 	}
 
 	s.Remove('b')
@@ -91,9 +101,19 @@ func TestSet(t *testing.T) {
 
 	s.Add('a')
 	s.Add('b')
+	str = fmt.Sprint(s)
+	if str != fmt.Sprintf("{%v, %v}", 'a', 'b') && str != fmt.Sprintf("{%v, %v}", 'b', 'a') {
+		t.Errorf("String() == %q, expected %q or %q",
+			str, fmt.Sprintf("{%v, %v}", 'a', 'b'), fmt.Sprintf("{%v, %v}", 'b', 'a'),
+		)
+	}
+
 	sClone := s.Clone()
 
 	s.Clear()
+	if Equals(s, sClone) {
+		t.Errorf("Equals(%v, %v) == true, expected false", s, sClone)
+	}
 	i = s.Len()
 	if i != 0 {
 		t.Errorf("Len() == %d, expected 0", i)
@@ -130,7 +150,244 @@ func TestSet(t *testing.T) {
 	if len(bs) != 2 || bs[0] != 'a' || bs[1] != 'b' {
 		t.Errorf("All() == %q, expected \"ab\"", string(bs))
 	}
+
+	s.Add('b')
+	s.Add('c')
+	if Equals(s, sClone) {
+		t.Errorf("Equals(%v, %v) == true, expected false", s, sClone)
+	}
+
 	for range sClone.All() {
 		break
+	}
+}
+
+func TestIntersection(t *testing.T) {
+	cases := []struct {
+		x, y, expected string
+	}{
+		{
+			x: "", y: "", expected: "",
+		},
+		{
+			x: "a", y: "b", expected: "",
+		},
+		{
+			x: "a", y: "a", expected: "a",
+		},
+		{
+			x: "ab", y: "a", expected: "a",
+		},
+		{
+			x: "b", y: "ab", expected: "b",
+		},
+		{
+			x: "ab", y: "bc", expected: "b",
+		},
+		{
+			x: "abc", y: "ab", expected: "ab",
+		},
+		{
+			x: "bc", y: "abc", expected: "bc",
+		},
+	}
+	for _, c := range cases {
+		sx := NewSet[byte]()
+		for i := range c.x {
+			sx.Add(c.x[i])
+		}
+		sy := NewSet[byte]()
+		for i := range c.y {
+			sy.Add(c.y[i])
+		}
+		se := NewSet[byte]()
+		for i := range c.expected {
+			se.Add(c.expected[i])
+		}
+		z := Intersection(sx, sy)
+		if !Equals(z, se) {
+			t.Errorf("Intersection(%v, %v) == %v, expected %v", sx, sy, z, se)
+		}
+	}
+}
+
+func TestUnion(t *testing.T) {
+	cases := []struct {
+		x, y, expected string
+	}{
+		{
+			x: "", y: "", expected: "",
+		},
+		{
+			x: "a", y: "b", expected: "ab",
+		},
+		{
+			x: "a", y: "a", expected: "a",
+		},
+		{
+			x: "ab", y: "a", expected: "ab",
+		},
+		{
+			x: "b", y: "ab", expected: "ab",
+		},
+		{
+			x: "ab", y: "bc", expected: "abc",
+		},
+		{
+			x: "abc", y: "ab", expected: "abc",
+		},
+		{
+			x: "bc", y: "abc", expected: "abc",
+		},
+	}
+	for _, c := range cases {
+		sx := NewSet[byte]()
+		for i := range c.x {
+			sx.Add(c.x[i])
+		}
+		sy := NewSet[byte]()
+		for i := range c.y {
+			sy.Add(c.y[i])
+		}
+		se := NewSet[byte]()
+		for i := range c.expected {
+			se.Add(c.expected[i])
+		}
+		z := Union(sx, sy)
+		if !Equals(z, se) {
+			t.Errorf("Union(%v, %v) == %v, expected %v", sx, sy, z, se)
+		}
+	}
+}
+
+func TestDifference(t *testing.T) {
+	cases := []struct {
+		x, y, expected string
+	}{
+		{
+			x: "", y: "", expected: "",
+		},
+		{
+			x: "a", y: "b", expected: "a",
+		},
+		{
+			x: "a", y: "a", expected: "",
+		},
+		{
+			x: "ab", y: "a", expected: "b",
+		},
+		{
+			x: "b", y: "ab", expected: "",
+		},
+		{
+			x: "ab", y: "bc", expected: "a",
+		},
+		{
+			x: "abc", y: "ab", expected: "c",
+		},
+		{
+			x: "bc", y: "abc", expected: "",
+		},
+	}
+	for _, c := range cases {
+		sx := NewSet[byte]()
+		for i := range c.x {
+			sx.Add(c.x[i])
+		}
+		sy := NewSet[byte]()
+		for i := range c.y {
+			sy.Add(c.y[i])
+		}
+		se := NewSet[byte]()
+		for i := range c.expected {
+			se.Add(c.expected[i])
+		}
+		z := Difference(sx, sy)
+		if !Equals(z, se) {
+			t.Errorf("Difference(%v, %v) == %v, expected %v", sx, sy, z, se)
+		}
+	}
+}
+
+func TestSymmetricDifference(t *testing.T) {
+	cases := []struct {
+		x, y, expected string
+	}{
+		{
+			x: "", y: "", expected: "",
+		},
+		{
+			x: "a", y: "b", expected: "ab",
+		},
+		{
+			x: "a", y: "a", expected: "",
+		},
+		{
+			x: "ab", y: "a", expected: "b",
+		},
+		{
+			x: "b", y: "ab", expected: "a",
+		},
+		{
+			x: "ab", y: "bc", expected: "ac",
+		},
+		{
+			x: "abc", y: "ab", expected: "c",
+		},
+		{
+			x: "bc", y: "abc", expected: "a",
+		},
+	}
+	for _, c := range cases {
+		sx := NewSet[byte]()
+		for i := range c.x {
+			sx.Add(c.x[i])
+		}
+		sy := NewSet[byte]()
+		for i := range c.y {
+			sy.Add(c.y[i])
+		}
+		se := NewSet[byte]()
+		for i := range c.expected {
+			se.Add(c.expected[i])
+		}
+		z := SymmetricDifference(sx, sy)
+		if !Equals(z, se) {
+			t.Errorf("SymmetricDifference(%v, %v) == %v, expected %v", sx, sy, z, se)
+		}
+	}
+}
+
+func TestCartesianProduct(t *testing.T) {
+	x := NewSet[byte]()
+	y := NewSet[byte]()
+	z := CartesianProduct(x, y)
+	expected := NewSet[[2]byte]()
+	if !Equals(z, expected) {
+		t.Errorf("CartesianProduct(%v, %v) == %v, expected %v", x, y, z, expected)
+	}
+
+	x.Add('a')
+	z = CartesianProduct(x, y)
+	if !Equals(z, expected) {
+		t.Errorf("CartesianProduct(%v, %v) == %v, expected %v", x, y, z, expected)
+	}
+
+	y.Add('b')
+	z = CartesianProduct(x, y)
+	expected.Add([2]byte{'a', 'b'})
+	if !Equals(z, expected) {
+		t.Errorf("CartesianProduct(%v, %v) == %v, expected %v", x, y, z, expected)
+	}
+
+	x.Add('b')
+	y.Add('a')
+	z = CartesianProduct(x, y)
+	expected.Add([2]byte{'a', 'a'})
+	expected.Add([2]byte{'a', 'b'})
+	expected.Add([2]byte{'b', 'a'})
+	expected.Add([2]byte{'b', 'b'})
+	if !Equals(z, expected) {
+		t.Errorf("CartesianProduct(%v, %v) == %v, expected %v", x, y, z, expected)
 	}
 }
