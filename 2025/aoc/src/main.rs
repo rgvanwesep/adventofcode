d11b6feffb1644ef02ee25f39264117815f19c6e
rgvanwesep <rvanwes@gmail.com>
2025-12-02 11:20:14 -0800

    feat: finish 2025 day 1 (#5)

---
+++ 2025/aoc/src/main.rs
@@ -0,0 +1,41 @@
+use aoc::RotationSeq;
+use clap::Parser;
+use std::error::Error;
+use std::io;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    day: u8,
+
+    #[arg(short, long)]
+    part: u8,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
+    let mut inputs = Vec::new();
+    for line in io::stdin().lines() {
+        inputs.push(line.unwrap())
+    }
+
+    match (args.day, args.part) {
+        (1, 1) => {
+            println!(
+                "Result: {}",
+                RotationSeq::build(inputs.iter().map(String::as_str).collect())?.count_zeros()
+            );
+            Ok(())
+        }
+        (1, 2) => {
+            println!(
+                "Result: {}",
+                RotationSeq::build(inputs.iter().map(String::as_str).collect())?.count_all_zeros()
+            );
+            Ok(())
+        }
+        _ => Err(format!("No match for Day {}, Part {}", args.day, args.part).into()),
+    }
+}
