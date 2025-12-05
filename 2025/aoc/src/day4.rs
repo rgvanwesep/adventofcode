f5bdc4593317d2e194be3cb3bd291ea40ef640a5
rgvanwesep <rvanwes@gmail.com>
2025-12-05 22:54:21 +0000

    feat: finish part 1

--- 2025/aoc/src/day4.rs
+++ 2025/aoc/src/day4.rs
@@ -1,5 +1,46 @@
+use ndarray::Array2;
+
+const ROLL: u8 = b'@';
+const EMPTY: u8 = b'.';
+
 pub fn count_rolls(inputs: Vec<&str>) -> u32 {
-    0
+    let (nrows, ncols, grid) = build_grid(inputs);
+    let mut count = 0;
+    let mut nneighbors: u8;
+    for i in 0..nrows {
+        for j in 0..ncols {
+            if grid[[i + 1, j + 1]] == ROLL {
+                nneighbors = 0;
+                for k in 0..3 {
+                    for l in 0..3 {
+                        if !(k == 1 && l == 1) && grid[[i + k, j + l]] == ROLL {
+                            nneighbors += 1;
+                        }
+                    }
+                }
+                if nneighbors < 4 {
+                    count += 1
+                }
+            }
+        }
+    }
+    count
+}
+
+fn build_grid(inputs: Vec<&str>) -> (usize, usize, Array2<u8>) {
+    let nrows = inputs.len();
+    let ncols = inputs[0].len();
+    let mut grid = Array2::from_elem((nrows + 2, ncols + 2), EMPTY);
+    let mut row: &[u8];
+    for i in 0..nrows {
+        row = inputs[i].as_bytes();
+        for j in 0..ncols {
+            if row[j] == ROLL {
+                grid[[i + 1, j + 1]] = ROLL;
+            }
+        }
+    }
+    (nrows, ncols, grid)
 }
 
 #[cfg(test)]
@@ -21,5 +62,46 @@ mod tests {
             "@.@.@@@.@.",
         ];
         assert_eq!(count_rolls(inputs), 13);
+    }
+
+    #[test]
+    fn count_rolls_small_zero() {
+        let inputs = vec!["...", ".@.", "..."];
+        assert_eq!(count_rolls(inputs), 1);
+    }
+
+    #[test]
+    fn count_rolls_small_two() {
+        let inputs = vec!["@..", ".@.", "..."];
+        assert_eq!(count_rolls(inputs), 2);
+    }
+
+    #[test]
+    fn count_rolls_small_three() {
+        let inputs = vec!["@@.", ".@.", "..."];
+        assert_eq!(count_rolls(inputs), 3);
+    }
+
+    #[test]
+    fn count_rolls_small_four() {
+        let inputs = vec!["@@@", ".@.", "..."];
+        assert_eq!(count_rolls(inputs), 4);
+    }
+
+    #[test]
+    fn count_rolls_small_five() {
+        let inputs = vec!["@@@", ".@@", "..."];
+        assert_eq!(count_rolls(inputs), 3);
+    }
+    #[test]
+    fn count_rolls_small_six() {
+        let inputs = vec!["@@@", ".@@", "..@"];
+        assert_eq!(count_rolls(inputs), 3);
+    }
+
+    #[test]
+    fn count_rolls_small_seven() {
+        let inputs = vec!["@@@", ".@@", ".@@"];
+        assert_eq!(count_rolls(inputs), 4);
     }
 }
