1bb29e83f5b99ce87ccbc3e3185507d8ed7ebf15
rgvanwesep <rvanwes@gmail.com>
2025-12-05 21:01:55 +0000

    feat: finish 2025 day 3 part 2

--- 2025/aoc/src/day3.rs
+++ 2025/aoc/src/day3.rs
@@ -1,33 +1,41 @@
-pub fn sum_joltage(inputs: Vec<&str>) -> i64 {
+use ndarray::Array2;
+
+pub fn sum_joltage(inputs: Vec<&str>, ndigits: usize) -> u64 {
     let mut sum = 0;
     for input in inputs {
-        sum += find_largest_joltage(input)
+        sum += find_largest_joltage(input, ndigits)
     }
     sum
 }
 
-fn find_largest_joltage(input: &str) -> i64 {
-    let bytes = input.as_bytes();
-    let len = bytes.len();
-    let mut max_after = vec![0; len - 1];
-    max_after[len - 2] = bytes[len - 1];
-    let mut max = bytes[len - 2];
-    let mut max_index = len - 2;
-    for i in (0..len - 2).rev() {
-        if bytes[i] > max {
-            max = bytes[i];
-            max_index = i;
-        } else if bytes[i] == max {
-            max_index = i;
-        }
-        if bytes[i + 1] > max_after[i + 1] {
-            max_after[i] = bytes[i + 1];
-        } else {
-            max_after[i] = max_after[i + 1];
+fn find_largest_joltage(input: &str, ndigits: usize) -> u64 {
+    let digits: Vec<u8> = input.as_bytes().iter().map(|x| x - '0' as u8).collect();
+    let len = digits.len();
+    let mut max_indices = Array2::<usize>::zeros((len, len));
+    for i in 0..len {
+        max_indices[[i, i]] = i;
+        for j in 1..len - i {
+            if digits[i + j] > digits[max_indices[[i, i + j - 1]]] {
+                max_indices[[i, i + j]] = i + j;
+            } else {
+                max_indices[[i, i + j]] = max_indices[[i, i + j - 1]];
+            }
         }
     }
-    max -= '0' as u8;
-    (max * 10 + max_after[max_index] - '0' as u8).into()
+
+    let mut j = 0;
+    let mut k = len - ndigits;
+    let mut index = max_indices[[j, k]];
+    let mut digit: u64 = digits[index].into();
+    let mut result = digit;
+    for i in 1..ndigits {
+        j = index + 1;
+        k = len - ndigits + i;
+        index = max_indices[[j, k]];
+        digit = digits[index].into();
+        result = result * 10 + digit;
+    }
+    result
 }
 
 #[cfg(test)]
@@ -35,18 +43,44 @@ mod tests {
     use super::*;
 
     #[test]
-    fn sum_joltage_example_input() {
+    fn sum_joltage_example_input_two() {
         let inputs = vec![
             "987654321111111",
             "811111111111119",
             "234234234234278",
             "818181911112111",
         ];
-        assert_eq!(sum_joltage(inputs), 357);
+        assert_eq!(sum_joltage(inputs, 2), 357);
     }
 
     #[test]
-    fn find_largest_joltage_small() {
-        assert_eq!(find_largest_joltage("12"), 12);
+    fn sum_joltage_example_input_twelve() {
+        let inputs = vec![
+            "987654321111111",
+            "811111111111119",
+            "234234234234278",
+            "818181911112111",
+        ];
+        assert_eq!(sum_joltage(inputs, 12), 3121910778619);
+    }
+
+    #[test]
+    fn find_largest_joltage_two_small() {
+        assert_eq!(find_largest_joltage("12", 2), 12);
+    }
+
+    #[test]
+    fn find_largest_joltage_two_large() {
+        assert_eq!(find_largest_joltage("818181911112111", 2), 92);
+    }
+
+    #[test]
+    fn find_largest_joltage_twelve_small() {
+        assert_eq!(find_largest_joltage("888911112111", 12), 888911112111);
+    }
+
+    #[test]
+    fn find_largest_joltage_twelve_large() {
+        assert_eq!(find_largest_joltage("818181911112111", 12), 888911112111);
     }
 }
