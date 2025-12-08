f93ffd010a6d721c0d87c51368345ce0dd943e67
rgvanwesep <rvanwes@gmail.com>
2025-12-08 18:53:42 +0000

    feat: finish part 1

--- 2025/aoc/src/day6.rs
+++ 2025/aoc/src/day6.rs
@@ -1,5 +1,27 @@
+use ndarray::{Array2, s};
+
 pub fn sum_results(inputs: Vec<&str>) -> u64 {
-    0
+    let nrows = inputs.len() - 1;
+    let ncols = inputs[0].split_whitespace().count();
+    let mut numbers = Array2::<u64>::zeros((nrows, ncols));
+    let mut sum = 0;
+    for (i, row) in inputs[..nrows].iter().enumerate() {
+        for (j, value) in row
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .enumerate()
+        {
+            numbers[[i, j]] = value;
+        }
+    }
+    for (i, op) in inputs[nrows].split_whitespace().enumerate() {
+        sum += match op {
+            "+" => numbers.slice(s![.., i]).sum(),
+            "*" => numbers.slice(s![.., i]).product(),
+            _ => panic!("Unknown operation!"),
+        }
+    }
+    sum
 }
 
 #[cfg(test)]
