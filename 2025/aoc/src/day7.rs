1658b4d566a467a4f9199b2140eb05ac3455e0e2
rgvanwesep <rvanwes@gmail.com>
2025-12-08 23:52:15 +0000

    feat: finish part 1

--- 2025/aoc/src/day7.rs
+++ 2025/aoc/src/day7.rs
@@ -1,5 +1,40 @@
+const SOURCE: u8 = b'S';
+const SPLITTER: u8 = b'^';
+const BEAM: u8 = b'|';
+
 pub fn count_splits(inputs: Vec<&str>) -> u32 {
     let mut count = 0;
+
+    let nrows = inputs.len();
+    let ncols = inputs[0].bytes().count();
+    let mut bytes = ndarray::Array2::<u8>::zeros((nrows, ncols));
+    for (i, row) in inputs.iter().enumerate() {
+        for (j, byte) in row.bytes().enumerate() {
+            bytes[[i, j]] = byte
+        }
+    }
+
+    for j in 0..ncols {
+        if bytes[[0, j]] == SOURCE {
+            bytes[[1, j]] = BEAM;
+            break;
+        }
+    }
+
+    for i in 2..nrows {
+        for j in 0..ncols {
+            if bytes[[i - 1, j]] == BEAM {
+                if bytes[[i, j]] == SPLITTER {
+                    count += 1;
+                    bytes[[i, j - 1]] = BEAM;
+                    bytes[[i, j + 1]] = BEAM;
+                } else {
+                    bytes[[i, j]] = BEAM;
+                }
+            }
+        }
+    }
+
     count
 }
 
