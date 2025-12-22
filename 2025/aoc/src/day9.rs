9f7f6b5a75d968f489d15a857729c28f97aa93d3
rgvanwesep <rvanwes@gmail.com>
2025-12-22 20:15:30 +0000

    feat: add block expansion

--- 2025/aoc/src/day9.rs
+++ 2025/aoc/src/day9.rs
@@ -1,3 +1,5 @@
+use std::cmp;
+
 pub fn largest_area(inputs: Vec<&str>) -> i64 {
     let points: Vec<Point2d> = inputs
         .iter()
@@ -24,9 +26,15 @@ pub fn largest_area_green_red(inputs: Vec<&str>) -> i64 {
     let (green_rows, green_cols) = build_green_rows_cols(&red_tiles);
     let interior_point = find_interior_point(&red_tiles, &green_cols);
 
-    let mut green_blocks = vec![(interior_point.clone(), interior_point.clone())];
-    let mut expansion_direction = Direction::Up;
-    // TODO: Implement green tile fill
+    let mut to_expand = vec![(interior_point.clone(), interior_point.clone())];
+    let mut block;
+    while !to_expand.is_empty() {
+        block = to_expand.pop().unwrap();
+        block.0.y = expand_up(&block, &red_tiles, &green_rows).unwrap_or(block.0.y);
+        block.1.y = expand_down(&block, &red_tiles, &green_rows).unwrap_or(block.1.y);
+        block.0.x = expand_left(&block, &red_tiles, &green_cols).unwrap_or(block.0.x);
+        block.1.x = expand_right(&block, &red_tiles, &green_cols).unwrap_or(block.1.x);
+    }
 
     let mut largest = 0;
     let mut current;
@@ -143,6 +151,102 @@ fn find_interior_point(red_tiles: &Vec<Point2d>, green_cols: &Vec<(Point2d, Poin
         }
     }
     interior_point
+}
+
+fn expand_up(
+    block: &(Point2d, Point2d),
+    red_tiles: &Vec<Point2d>,
+    green_rows: &Vec<(Point2d, Point2d)>,
+) -> Option<i64> {
+    let closest_intersecting_red_tile = red_tiles
+        .iter()
+        .filter(|point| point.y < block.0.y && point.x == block.0.x)
+        .max_by_key(|point| point.y);
+    let closest_intersecting_green_row = green_rows
+        .iter()
+        .filter(|(start, end)| start.y < block.0.y && start.x <= block.0.x && end.x >= block.0.x)
+        .max_by_key(|(start, _)| start.y);
+    match (
+        closest_intersecting_red_tile,
+        closest_intersecting_green_row,
+    ) {
+        (Some(red_tile), Some(green_row)) => Some(cmp::max(red_tile.y, green_row.0.y)),
+        (Some(red_tile), None) => Some(red_tile.y),
+        (None, Some(green_row)) => Some(green_row.0.y),
+        (None, None) => None,
+    }
+}
+
+fn expand_down(
+    block: &(Point2d, Point2d),
+    red_tiles: &Vec<Point2d>,
+    green_rows: &Vec<(Point2d, Point2d)>,
+) -> Option<i64> {
+    let closest_intersecting_red_tile = red_tiles
+        .iter()
+        .filter(|point| point.y > block.1.y && point.x == block.1.x)
+        .min_by_key(|point| point.y);
+    let closest_intersecting_green_row = green_rows
+        .iter()
+        .filter(|(start, end)| start.y > block.1.y && start.x <= block.1.x && end.x >= block.1.x)
+        .min_by_key(|(start, _)| start.y);
+    match (
+        closest_intersecting_red_tile,
+        closest_intersecting_green_row,
+    ) {
+        (Some(red_tile), Some(green_row)) => Some(cmp::min(red_tile.y, green_row.0.y)),
+        (Some(red_tile), None) => Some(red_tile.y),
+        (None, Some(green_row)) => Some(green_row.0.y),
+        (None, None) => None,
+    }
+}
+
+fn expand_left(
+    block: &(Point2d, Point2d),
+    red_tiles: &Vec<Point2d>,
+    green_cols: &Vec<(Point2d, Point2d)>,
+) -> Option<i64> {
+    let closest_intersecting_red_tile = red_tiles
+        .iter()
+        .filter(|point| point.x < block.0.x && point.y >= block.0.y && point.y <= block.1.y)
+        .max_by_key(|point| point.x);
+    let closest_intersecting_green_col = green_cols
+        .iter()
+        .filter(|(start, end)| start.x < block.0.x && start.y <= block.1.y && end.y >= block.0.y)
+        .max_by_key(|(start, _)| start.x);
+    match (
+        closest_intersecting_red_tile,
+        closest_intersecting_green_col,
+    ) {
+        (Some(red_tile), Some(green_col)) => Some(cmp::max(red_tile.x, green_col.0.x)),
+        (Some(red_tile), None) => Some(red_tile.x),
+        (None, Some(green_col)) => Some(green_col.0.x),
+        (None, None) => None,
+    }
+}
+
+fn expand_right(
+    block: &(Point2d, Point2d),
+    red_tiles: &Vec<Point2d>,
+    green_cols: &Vec<(Point2d, Point2d)>,
+) -> Option<i64> {
+    let closest_intersecting_red_tile = red_tiles
+        .iter()
+        .filter(|point| point.x > block.1.x && point.y >= block.0.y && point.y <= block.1.y)
+        .min_by_key(|point| point.x);
+    let closest_intersecting_green_col = green_cols
+        .iter()
+        .filter(|(start, end)| start.x > block.0.x && start.y <= block.1.y && end.y >= block.0.y)
+        .min_by_key(|(start, _)| start.x);
+    match (
+        closest_intersecting_red_tile,
+        closest_intersecting_green_col,
+    ) {
+        (Some(red_tile), Some(green_col)) => Some(cmp::min(red_tile.x, green_col.0.x)),
+        (Some(red_tile), None) => Some(red_tile.x),
+        (None, Some(green_col)) => Some(green_col.0.x),
+        (None, None) => None,
+    }
 }
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
